ee167cdcd2e6df44ac6fdbf759b4a760775e92a5
fanes-setiawan <fanessetiawa.1401@gmail.com>
2024-10-23 13:35:35 +0700

    p5 : relationship lavel-user

--- app.py
+++ app.py
@@ -3,16 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# books = {
-#     1:{'id':1, 'title' : 'Python basic', 'author':'John doe' , 'year' :2020},
-#     2:{'id':2, 'title' : 'Flask for Beginners', 'author':'John Smith' , 'year' :2021}
-# }
-
-# users ={
-#     1:{'user_id' :1 ,'username':'fanes123' , 'password' :'admin123' ,'fullname':'fanes setiawan','status':'mahasiswa'},
-#     2:{'user_id' :2 ,'username':'admin321' , 'password' :'321admin' ,'fullname':'adminadmin','status':'admin'}
-# }
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://pwl123:2wsx1qaz@localhost:3306/db_repository_0627'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -43,6 +33,7 @@ class User(db.Model):
     password = db.Column(db.String(100), nullable =False)
     fullname = db.Column(db.String(100), nullable =False)
     status = db.Column(db.String(100) , nullable =False)
+    level_id = db.Column(db.Integer ,db.ForeignKey('level.id'), nullable =False)
     
     def to_dict(self):
         return {
@@ -50,7 +41,8 @@ class User(db.Model):
             'username': self.username,
             'fullname': self.fullname,
             'password' : self.password,
-            'status': self.status
+            'status': self.status,
+            'level_id': self.level_id
         }
 
 class Category(db.Model):
@@ -64,12 +56,27 @@ class Category(db.Model):
             'id':self.id,
             'name':self.name
         }
+        
+class Level(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False,unique=True)
+    
+    users = db.relationship('User',backref='level',lazy=True)
+    
+    def to_dict(self):
+        return{
+            'id':self.id,
+            'name':self.name
+        }
 
 #init database
 @app.before_request
 def create_tables():
     app.before_request_funcs[None].remove(create_tables)
     db.create_all()
+
+
+#BOOK ROUTE==========================================================================>
 
 #get all book
 @app.route('/books',methods=['GET'])
@@ -194,13 +201,34 @@ def delete_book(book_id):
     db.session.commit()
     return jsonify({'message':'Book deleted successfully!'})
 
-#<----- user ----->
+#USER ROUTE==========================================================================>
 
 #get all user
 @app.route('/users',methods=['GET'])
 def get_users():
     users = User.query.all()
-    return jsonify([user.to_dict() for user in users])
+    users_with_level =[]
+    for user in users:
+        #get level
+        level = Level.query.get(user.level_id)
+        #add details
+        users_with_level.append({
+            'id': user.id,
+            'username': user.username,
+            'fullname': user.fullname,
+            'password' : user.password,
+            'status': user.status,
+            'level_name': level.name if level else "No Level"
+        })
+        
+    response ={
+        'status':'success',
+        'data':{
+            'users':users_with_level
+        },
+        'message':'Users retrived successfully!'
+    }
+    return jsonify(response),200
 
 #get by id
 @app.route('/users/<int:user_id>',methods=['GET'])
@@ -208,7 +236,26 @@ def get_user(user_id):
     user = User.query.get(user_id)
     if not user:
         return jsonify({'error' : 'user not found'}),404
-    return jsonify(user.to_dict())
+    
+    #get level
+    level = Level.query.get(user.level_id)
+    user_data = {
+        'id': user.id,
+        'username': user.username,
+        'fullname': user.fullname,
+        'password' : user.password,
+        'status': user.status,
+        'level_name': level.name if level else "No Level"
+    }
+    
+    response ={
+        'status':'success',
+        'data':{
+            'user':user_data
+        },
+        'message':'User retrieved successfuly!'
+    }
+    return jsonify(response),200
 
 #add new user
 @app.route('/users',methods=['POST'])
@@ -218,7 +265,8 @@ def add_user():
         username = new_user_data['username'],
         password = new_user_data['password'],
         fullname = new_user_data['fullname'],
-        status = new_user_data['status']
+        status = new_user_data['status'],
+        level_id = new_user_data['level_id']
     )
     db.session.add(new_user)
     db.session.commit()
@@ -235,6 +283,7 @@ def update_user(user_id):
     user.password = update_data.get('password',user.password)
     user.fullname = update_data.get('fullname',user.fullname)
     user.status = update_data.get('status',user.status)
+    user.level_id = update_data.get('level_id',user.level_id)
     
     db.session.commit()
     return jsonify({'message' : 'user update successfully!','user' :user.to_dict()})
@@ -258,6 +307,12 @@ def patch_user(user_id):
         
     if 'status' in patch_data:
         user.status = patch_data['status']
+    if 'level' in patch_data:
+        level = Level.query.get(patch_data['level_id'])
+        if level:
+            user.level_id = patch_data['level_id']
+        else:
+            return jsonify({'error': 'Level not found'}),404
         
     db.session.commit()
     return jsonify({'message':'user partially updated successfully!','user':user.to_dict()})
@@ -274,7 +329,7 @@ def delete_user(user_id):
     return jsonify({'message':'user deleted successfully!'})
 
 
-#CATEGORY ROUTE
+#CATEGORY ROUTE==========================================================================>
 
 #get all category
 @app.route('/category',methods=['GET'])
@@ -309,7 +364,8 @@ def update_category(id):
         return jsonify({'error':'Category not found'}),404
     updated_data = request.get_json()
     category.name = updated_data.get('name',category.name)
-    return jsonify({'message':'Category updated succesfully!' , 'category':category.to_disct()})
+    db.session.commit()
+    return jsonify({'message':'Category updated succesfully!' , 'category':category.to_dict()})
 
 #delete a category (DELETE)
 @app.route('/category/<int:id>',methods=['DELETE'])
@@ -320,6 +376,57 @@ def delete_category(id):
     db.session.delete(category)
     db.session.commit()
     return jsonify({'message':'Category deleted successfully!'})
+
+#LEVEL ROUTE==========================================================================>
+
+#get all level
+@app.route('/level',methods=['GET'])
+def get_levels():
+    levels= Level.query.all()
+    return jsonify([level.to_dict() for level in levels])
+
+#get level by id
+@app.route('/level/<int:id>',methods=['GET'])
+def get_level(id):
+    level = Level.query.get(id)
+    if not level:
+        return jsonify({'error':'Level not found'}),404
+    return jsonify(level.to_dict())
+
+#new level
+@app.route('/level',methods=['POST'])
+def add_level():
+    new_level_data = request.get_json()
+    new_level = Level(
+        name = new_level_data['name']
+    )
+    db.session.add(new_level)
+    db.session.commit()
+    return jsonify({'message':'Level added succesfully!' ,'level':new_level.to_dict()}),201
+
+#update level (PUT)
+@app.route('/level/<int:id>',methods=['PUT'])
+def update_level(id):
+    level = Level.query.get(id)
+    if not level:
+        return jsonify({'error':'Level not found'}),404
+    
+    update_data = request.get_json()
+    level.name = update_data.get('name',level.name)
+    db.session.commit()
+    return jsonify({'message':'Level update successfully!','level':level.to_dict()})
+
+#delete level
+@app.route('/level/<int:id>',methods=['DELETE'])
+def delete_level(id):
+    level = Level.query.get(id)
+    if not level:
+        return jsonify({'error':'Level not found'}),404
+    db.session.delete(level)
+    db.session.commit()
+    return jsonify({'message':'Level deleted successfullt!'})
+    
+
 
 @app.errorhandler(404)
 def not_found(error):
