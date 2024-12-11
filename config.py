ce8ecdad2639bde677e8f81b67d2cabcf2e3f1a3
fanes-setiawan <fanessetiawa.1401@gmail.com>
2024-12-11 21:00:41 +0700

    p10 : flask_cors

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from flask_cors import CORS
 
 #init db connect
 app = Flask(__name__)
+CORS(app,resources={r"/api/*": {"origins": "*"}},supports_credentials=True)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://flaskapiPractices_foreigndug:f160a689adba286f94b25abbc358ab59ac99f42b@junbl.h.filess.io:3305/flaskapiPractices_foreigndug'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
